a392111ad5cfb99a6bb2774c9a045640ff1453e7
mahidulkabir <mahidulkabir8@gmail.com>
2025-03-07 12:04:35 +0600

    second commit

---
+++ slice.js
@@ -0,0 +1,8 @@
+const goal = "assala mu alaikum";
+let end ;
+end = goal.slice(4,11);
+console.log(end);
+endd= goal.substring(4,11);
+console.log(endd);
+
+// slice e bujhay 4 theke suru kore 11 er ag prjnto slice kore nao 
