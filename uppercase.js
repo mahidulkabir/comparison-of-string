57645b36fe0b3acbe5c42bff1c8d42f5ea5ef07b
mahidulkabir <mahidulkabir8@gmail.com>
2025-03-07 11:17:14 +0600

    first commit

---
+++ uppercase.js
@@ -0,0 +1,18 @@
+const school = 'Rajuk Utt ara Model school';
+// console.log(school);
+
+const subject = "chemistry ";
+const book = " CheMistry";
+
+if (subject.toLowerCase().trim() === book.toLowerCase().trim()){
+    console.log("yoo ! let's gooo");
+}
+else {
+    console.log('amar boi koi');
+}
+
+console.log(subject);
+console.log(book);
+
+// compare korte gele eta important. lower case diye sob gulake ek stage e niye ase . tarpor trim diye age piche white space gula bad diye Dey . 
+// evabe comparison kortee hoy string er. 
